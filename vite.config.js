831203075c800462ae60750d6f46e74ce9d4f8be
fdoganis <fivos.doganis@gmail.com>
2025-09-11 00:39:21 +0200

    build(vite): fix root path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-    base: "/three_vite_xr_js13k",
+    base: "/clawz",
     clearScreen: false,
     optimizeDeps: {
         esbuildOptions: {
