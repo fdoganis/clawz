dc0d1ee3b0fc8c07f9633ac96957a84a568aa3b3
fdoganis <fivos.doganis@gmail.com>
2025-09-14 19:37:17 +0200

    build: add local copy of THREE.js 179 for testing in dev mode

--- vite.config.js
+++ vite.config.js
@@ -1,57 +1,69 @@
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import glsl from 'vite-plugin-glsl';
-import { viteSingleFile } from "vite-plugin-singlefile"
+import { viteSingleFile } from "vite-plugin-singlefile";
 
-export default defineConfig({
-    base: "/clawz",
-    clearScreen: false,
-    optimizeDeps: {
-        esbuildOptions: {
+export default defineConfig(({ mode }) => {
+
+    //const THREE_CDN_PATH = 'https://cdn.jsdelivr.net/npm/three@0.179.0/build/three.module.js';
+    const THREE_CDN_PATH = 'https://js13kgames.com/2025/webxr/three.module.js';
+    const THREE_LOCAL_PATH = '/three.module.js';
+
+    return {
+        base: "/clawz",
+        clearScreen: false,
+        optimizeDeps: {
+            esbuildOptions: {
+                supported: {
+                    'top-level-await': true
+                }
+            }
+        },
+        esbuild: {
             supported: {
                 'top-level-await': true
             }
         }
-    },
-    esbuild: {
-        supported: {
-            'top-level-await': true
-        }
-    }
-    ,
-    /*
-    build: {
-        sourcemap: true,
-        chunkSizeWarningLimit: 1024,
-        minify: false,
-        terserOptions: {
-            compress: false,
-            mangle: false
-        }
-    }
-    ,*/
-    build: {
-        sourcemap: false,
-        chunkSizeWarningLimit: 1024,
-        minify: true,
-        terserOptions: {
-            compress: true,
-            mangle: true
+        ,
+        resolve: {
+            alias: {
+                'three': mode === 'production' ? 'three' : THREE_LOCAL_PATH
+            }
         },
-        rollupOptions: {
-            external: ['three'],
-            output: {
-                paths: {
-                    three: 'https://js13kgames.com/2025/webxr/three.module.js'
+        /*
+        build: {
+            sourcemap: true,
+            chunkSizeWarningLimit: 1024,
+            minify: false,
+            terserOptions: {
+                compress: false,
+                mangle: false
+            }
+        }
+        ,*/
+        build: {
+            sourcemap: mode === 'production' ? false : true,
+            chunkSizeWarningLimit: 1024,
+            minify: mode === 'production' ? true : false,
+            terserOptions: {
+                compress: mode === 'production' ? true : false,
+                mangle: mode === 'production' ? true : false
+            },
+            rollupOptions: {
+                external: ['three'],
+                output: {
+                    paths: {
+                        three: mode === 'production' ? THREE_CDN_PATH : 'three'
+                    }
                 }
             }
         }
-    }
-    ,
-    server: {
-        open: true,
-        allowedHosts: ['.trycloudflare.com']
-    },
-    plugins: [viteSingleFile()]
-})
+        ,
+        server: {
+            open: true,
+            allowedHosts: ['.trycloudflare.com']
+        },
+        plugins: [viteSingleFile()]
+    };
+});
 
